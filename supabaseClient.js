24966f1fd277bcec30bcd0308f5b1fb69780bf73
Zhonghe-packing <zwcchatgpt2@outlook.com>
2026-09-11 22:25:35 +0800

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://ozctrkjtcsqacgqkospq.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_SCqqiMFdqhWSa_m8n1GfzA_Tdoin_da";
-
-
-window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
