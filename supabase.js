2a506acbcbf77cc1a57e780163bde91425cda18b
Zhonghe-packing <zwcchatgpt2@outlook.com>
2026-09-11 22:24:02 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ozctrkjtcsqacgqkospq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_SCqqiMFdqhWSa_m8n1GfzA_Tdoin_da";
+
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
